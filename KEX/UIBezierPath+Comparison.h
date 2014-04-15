70df12aaff1a6499fde8a45edce7fb8386452f6d
Daniel Schlaug <daniel.schlaug@gmail.com>
2014-04-15 13:03:33 +0200

    Added mean distance comparator for UIBezierPath
    Based on the shape contains point method

---
+++ KEX/UIBezierPath+Comparison.h
@@ -0,0 +1,13 @@
+//
+//  UIBezierPath+Comparison.h
+//  KEX
+//
+//  Created by Daniel Schlaug on 4/11/14.
+//  Copyright (c) 2014 Daniel Schlaug. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface UIBezierPath (Comparison)
+
+@end
---
+++ KEX/UIBezierPath+Comparison.m
@@ -0,0 +1,13 @@
+//
+//  UIBezierPath+Comparison.m
+//  KEX
+//
+//  Created by Daniel Schlaug on 4/11/14.
+//  Copyright (c) 2014 Daniel Schlaug. All rights reserved.
+//
+
+#import "UIBezierPath+Comparison.h"
+
+@implementation UIBezierPath (Comparison)
+
+@end
