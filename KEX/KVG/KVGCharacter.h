3f789bc908f09261626a6382894252229f59f114
Daniel Schlaug <daniel.schlaug@gmail.com>
2014-05-03 11:11:06 +0200

    Cleanup

--- KEX/KVG/KVGCharacter.h
+++ KEX/KVG/KVGCharacter.h
@@ -1,6 +1,5 @@
 //
-//  DSKanji.h
-//  KEX
+//  KVGCharacter.h
 //
 //  Created by Daniel Schlaug on 2/24/14.
 //  Copyright (c) 2014 Daniel Schlaug. All rights reserved.
@@ -10,24 +9,32 @@
 #import "KVGElement.h"
 #import <UIKit/UIKit.h>
 
-/** This class represents a KanjiVG character. It contains metadata about the character and a tree of the characters elements and leaf strokes.
- 
-    The class is a subclass of NSManagedObject and so can easily be stored in a CoreData database. See KVGCache for an implementation of such a database.
+/**
+ *  This class represents a KanjiVG character. It contains metadata about the character and a tree of the characters elements and leaf strokes.
  */
 
 @interface KVGCharacter : NSObject
 
 @property (nonatomic, readonly) unichar character;
-@property (nonatomic, strong, readonly) KVGElement *element;
-
+@property (nonatomic, readonly, strong) KVGElement *element;
 
 + (id)characterFromSVGFile:(NSString *)filename;
 + (id)characterFromSVGData:(NSData *)data;
 
-/** Recurses through all the elements of the character to find all the strokes.
- @return An NSArray of KVGStroke objects representing all strokes of the character. Stroke order is preserved through the index of each stroke.
+/**
+ *  Recurses through all the elements of the character to find all the strokes.
+ *
+ *  @return An NSArray of KVGStroke objects representing all strokes of the character. Stroke order is preserved through the index of each stroke.
  */
 - (NSArray *)strokes;
 
+/**
+ *  <#Description#>
+ *
+ *  @param strokeCount <#strokeCount description#>
+ *
+ *  @return <#return value description#>
+ */
+- (KVGStroke *)strokeWithStrokeCount:(NSInteger)strokeCount;
 
 @end
